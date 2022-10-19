3e961467922175c2b5d99c3fc18ccd506903a778
Guilherme Origo fulop <103580606+GuilhermeFulop@users.noreply.github.com>
2022-10-19 09:30:21 -0300

    Update main.py

--- Deploy/main.py
+++ Deploy/main.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 import pickle
 import json
-from sklearn.ensemble import RandomForestClassifier
+
 
 ## Creating the header of our App ##
 
